ff8bf6868a982592a8eacc724bc565f1dd82d4ff
yangqinkuan <male_2@163.com>
2018-12-05 23:42:03 +0800

    aaa

---
+++ find-util/src/main/java/codegenerate/ValidCode.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2018-2018,
+ * FileName: ValidCode
+ * Author:   Administrator
+ * Date:     2018/12/5 23:20
+ * Description: get valid code
+ */
+package codegenerate;
+
+import java.util.Random;
+
+public class ValidCode {
+
+    public String get6Code(){
+        Random rand = new Random();
+        rand.nextInt(10);
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
