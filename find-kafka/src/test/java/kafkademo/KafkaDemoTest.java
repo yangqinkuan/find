f201a71e3647ea3a17f6caf66518ecb1db995e98
yangqinkuan <male_2@163.com>
2019-01-09 18:00:43 +0800

    增加日志和错误类处理

--- find-kafka/src/test/java/kafkademo/KafkaDemoTest.java
+++ find-kafka/src/test/java/kafkademo/KafkaDemoTest.java
@@ -13,6 +13,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,7 +22,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KafkaApp.class)
 public class KafkaDemoTest {
-
+    private static Logger logger = LoggerFactory.getLogger(KafkaDemoTest.class);
     @Autowired
     private KafkaSenderImpl kafkaSenderImpl;
 
